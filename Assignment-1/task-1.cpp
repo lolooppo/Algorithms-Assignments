d00ecbbc280ed3fe7ec1e00137d866b2ee3eb260
Alaa_Omran <99070234+lolooppo@users.noreply.github.com>
2024-10-25 23:56:13 +0300

    Update task-1.cpp

--- Assignment-1/task-1.cpp
+++ Assignment-1/task-1.cpp
@@ -6,37 +6,27 @@
 /*          Fibonacci functions        */
 /***************************************/
 long long fib_recursion(int n){
-    //ensure it is 1-based
-    assert(n>0);
-
-    //0 for the first and 1 for the second in fib series.
-    if(n==1 or n==2)
-        return n-1;
+    if(n==0 or n==1)
+        return n;
     return fib_recursion(n-1) + fib_recursion(n-2);
 }
 
 long long fib_dynamic_programming(int n){
-    //ensure it is 1-based
-    assert(n>0);
-
-    if(n == 1 or n == 2)
-        return n-1;
+    if(n == 0 or n == 1)
+        return n;
 
     std::vector<long long> fib(n+1, 0);
 
-    fib[1] = 0;
-    fib[2] = 1;
+    fib[0] = 0;
+    fib[1] = 1;
 
-    for(int i = 3; i<= n; i++){
+    for(int i = 2; i<= n; i++){
         fib[i] = fib[i-1] + fib[i-2];
     }
 
     return fib[n];
 }
 
-
 int main(){
-
-    //  Test the functions here
-
+    
 }
